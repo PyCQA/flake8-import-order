dd5670f3659a46c956471c5a9a7093996d2be446
Alex Stapleton <alexs@prol.etari.at>
2014-03-18 15:02:54 +0000

    Pylama tests

---
+++ tests/test_pylama_linter.py
@@ -0,0 +1,48 @@
+import ast
+import os
+
+import pytest
+
+from flake8_import_order import pylama_linter
+
+
+def load_test_cases():
+    base_path = os.path.dirname(__file__)
+    test_case_path = os.path.join(base_path, "test_cases")
+    test_case_files = os.listdir(test_case_path)
+
+    test_cases = []
+
+    for fname in test_case_files:
+        if not fname.endswith(".py"):
+            continue
+
+        fullpath = os.path.join(test_case_path, fname)
+        data = open(fullpath).read()
+
+        lines = data.splitlines()
+
+        expected = [
+            line.split()[1]
+            for line in lines
+            if line.startswith("# ")
+        ]
+
+        test_cases.append((fullpath, expected))
+
+    return test_cases
+
+
+@pytest.mark.parametrize(
+    "filename, expected",
+    load_test_cases()
+)
+def test_expected_error(filename, expected):
+    checker = pylama_linter.Linter()
+    assert checker.allow(filename)
+
+    errors = []
+    for error in checker.run(filename):
+        code = error['type']
+        errors.append(code)
+    assert errors == expected
