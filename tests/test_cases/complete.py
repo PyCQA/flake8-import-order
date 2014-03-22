764e77b64ea21422084d68f9678468941859a449
Alex Stapleton <alexs@prol.etari.at>
2014-03-22 09:07:01 +0000

    A test case that covers many ideal examples

---
+++ tests/test_cases/complete.py
@@ -0,0 +1,18 @@
+import ast
+from functools import *
+import os
+from os import path
+import sys
+
+import X
+from X import *
+from X import A
+from X import B, C, D
+
+import Y
+from Y import *
+from Y import A
+from Y import B, C, D
+
+import flake8_import_error
+from flake8_import_error import *
