61ca64d9468ba649d39514ea6b8b2a3e6623c6e3
Alex Stapleton <alex@lyst.com>
2016-03-03 17:13:43 +0000

    Bump to 0.7

--- flake8_import_order/__about__.py
+++ flake8_import_order/__about__.py
@@ -1,15 +1,3 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from __future__ import absolute_import, division, print_function
 
 __all__ = [
@@ -23,10 +11,10 @@ __summary__ = (
 )
 __uri__ = "https://github.com/public/flake8-import-order"
 
-__version__ = "0.6.1"
+__version__ = "0.7"
 
 __author__ = "Alex Stapleton"
 __email__ = "alexs@prol.etari.at"
 
 __license__ = "LGPLv3"
-__copyright__ = "Copyright 2013-2015 %s" % __author__
+__copyright__ = "Copyright 2013-2016 %s" % __author__
