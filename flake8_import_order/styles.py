PyCQA/flake8-import-order

commit 7c04e334c6b03727e7dcc5cb50535fea14044ce6

Refactor structure
Flake8-Import-Order was originally written for a single style, namely
cryptography over time the google, smarkets and most recently pep8
styles have been added. These additions patched the styles into the
codebase, whereas this commit refactors the codebase to make differing
styles the norm.

This introduces a potentially breaking change to the cryptography
style, which is best seen in the complete_with_bad_order.py test
case. In this case Flake8-Import-Order would previously insist that
`from Z.A.B import A` be before `from Z import A` whereas now `from
Z.A import A` must be before `from Z.A.B import A`. This I believe is
the expected cryptography style.

Finally this drops support for Python2.6, to allow for set and dict
comprehension usage.

flake8_import_order/styles.py
@@ -1,0 +1,135 @@
+from collections import namedtuple
+
+from flake8_import_order import (
+    IMPORT_3RD_PARTY, IMPORT_APP, IMPORT_APP_RELATIVE,
+)
+
+RELATIVE_SET = {IMPORT_APP, IMPORT_APP_RELATIVE}
+
+Error = namedtuple('Error', ['lineno', 'code', 'message'])
+
+
+class Style(object):
+
+    def __init__(self, imports):
+        self.imports = imports
+
+    def check(self):
+        previous = None
+        for current in self.imports:
+            if current.type == -1:
+                yield Error(
+                    current.lineno,
+                    'I666',
+                    'Import statement mixes groups',
+                )
+
+            correct_names = self.sorted_names(current.names)
+            if correct_names != current.names:
+                corrected = ', '.join(correct_names)
+                yield Error(
+                    current.lineno,
+                    'I101',
+                    "Imported names are in the wrong order. "
+                    "Should be {0}".format(corrected),
+                )
+
+            if previous is not None:
+                if self.key(previous) > self.key(current):
+                    first = self._explain(current)
+                    second = self._explain(previous)
+                    yield Error(
+                        current.lineno,
+                        'I100',
+                        "Import statements are in the wrong order."
+                        "{0} should be before {1}".format(first, second),
+                    )
+
+                spacing = current.lineno - previous.lineno
+                if not self.same_section(previous, current) and spacing == 1:
+                    yield Error(
+                        current.lineno,
+                        'I201',
+                        'Missing newline before sections or imports.',
+                    )
+
+            previous = current
+
+    @staticmethod
+    def sorted_names(names):
+        return names
+
+    @staticmethod
+    def key(import_):
+        return (import_.type,)
+
+    @staticmethod
+    def same_section(previous, current):
+        same_type = current.type == previous.type
+        both_relative = {previous.type, current.type} <= RELATIVE_SET
+        return same_type or both_relative
+
+    @staticmethod
+    def _explain(import_):
+        if import_.is_from:
+            text = 'from ' + import_.level * '.'
+        else:
+            text = 'import '
+        return text + ', '.join(import_.modules)
+
+
+class PEP8(Style):
+    pass
+
+
+class Google(Style):
+
+    @staticmethod
+    def sorted_names(names):
+        return sorted(names, key=lambda n: n.lower())
+
+    @staticmethod
+    def key(import_):
+        return (import_.type, import_.level, import_.modules)
+
+
+class Smarkets(Style):
+
+    @staticmethod
+    def sorted_names(names):
+        return sorted(names, key=lambda n: n.lower())
+
+    @staticmethod
+    def key(import_):
+        return (import_.type, import_.is_from, import_.level, import_.modules)
+
+
+class Cryptography(Style):
+
+    @staticmethod
+    def sorted_names(names):
+        return sorted(names)
+
+    @staticmethod
+    def key(import_):
+        if import_.type in {IMPORT_3RD_PARTY, IMPORT_APP}:
+            return (
+                import_.type, import_.package, import_.is_from,
+                import_.level, import_.modules,
+            )
+        else:
+            return (
+                import_.type, '', import_.is_from, import_.level,
+                import_.modules,
+            )
+
+    @staticmethod
+    def same_section(previous, current):
+        app_or_third = current.type in {IMPORT_3RD_PARTY, IMPORT_APP}
+        same_type = current.type == previous.type
+        both_relative = {previous.type, current.type} <= RELATIVE_SET
+        same_package = previous.package == current.package
+        return (
+            (not app_or_third and same_type or both_relative) or
+            (app_or_third and same_package)
+        )

flake8_import_order/__init__.py
@@ -1,13 +1,11 @@
 import ast
-
-import pycodestyle
+from collections import namedtuple
 
 from flake8_import_order.__about__ import (
     __author__, __copyright__, __email__, __license__, __summary__, __title__,
-    __uri__, __version__
+    __uri__, __version__,
 )
 from flake8_import_order.stdlib_list import STDLIB_NAMES
-
 
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
@@ -23,6 +21,11 @@ IMPORT_APP = 30
 IMPORT_APP_RELATIVE = 40
 IMPORT_MIXED = -1
 
+ClassifiedImport = namedtuple(
+    'ClassifiedImport',
+    ['type', 'is_from', 'modules', 'names', 'lineno', 'level', 'package'],
+)
+
 
 def root_package_name(name):
     p = ast.parse(name)
@@ -33,284 +36,51 @@ def root_package_name(name):
         return None
 
 
-def is_sorted(seq):
-    return sorted(seq) == list(seq)
-
-
-def lower_strings(l):
-    if l is None:
-        return None
-    else:
-        return [e.lower() if hasattr(e, 'lower') else e for e in l]
-
-
-def sorted_import_names(names, style):
-    if style != 'cryptography':
-        sorted_names = sorted(names, key=lambda s: s[0].lower())
-    else:
-        sorted_names = sorted(names, key=lambda s: s[0])
-    return ", ".join(name[0] for name in sorted_names)
-
-
-def cmp_values(n, style):
-    if style != 'cryptography':
-        return [
-            n[0],
-            n[1],
-            lower_strings(n[2]),
-            n[3],
-            [lower_strings(x) for x in n[4]]
-        ]
-    else:
-        return n
-
-
 class ImportVisitor(ast.NodeVisitor):
-    """
-    This class visits all the import nodes at the root of tree and generates
-    sort keys for each import node.
 
-    In practice this means that they are sorted according to something like
-    this tuple.
-
-        (stdlib, site_packages, names)
-    """
-
-    def __init__(self, filename, options):
-        self.filename = filename
-        self.options = options or {}
+    def __init__(self, application_import_names):
         self.imports = []
-
-        self.application_import_names = set(
-            self.options.get("application_import_names", [])
-        )
-        self.style = self.options['import_order_style']
+        self.application_import_names = application_import_names
 
     def visit_Import(self, node):  # noqa
         if node.col_offset == 0:
-            self.imports.append(node)
+            modules = [alias.name for alias in node.names]
+            types_ = {self._classify_type(module) for module in modules}
+            if len(types_) == 1:
+                type_ = types_.pop()
+            else:
+                type_ = IMPORT_MIXED
+            classified_import = ClassifiedImport(
+                type_, False, modules, [], node.lineno, 0,
+                root_package_name(modules[0]),
+            )
+            self.imports.append(classified_import)
 
     def visit_ImportFrom(self, node):  # noqa
         if node.col_offset == 0:
-            self.imports.append(node)
-
-    def node_sort_key(self, node):
-        """
-        Return a key that will sort the nodes in the correct
-        order for the Google Code Style guidelines.
-        """
-
-        if isinstance(node, ast.Import):
-            names = [nm.name for nm in node.names]
-        elif isinstance(node, ast.ImportFrom):
-            names = [node.module or '']
-        else:
-            raise TypeError(type(node))
-
-        import_type = self._import_type(node, names[0])
-        for name in names[1:]:
-            name_type = self._import_type(node, name)
-            if import_type != name_type:
-                import_type = IMPORT_MIXED
-                break
-
-        imported_names = [
-            [nm.name if nm.name != "*"
-             else "{0}.*".format(node.module), nm.asname]
-            for nm in node.names
-        ]
-
-        if self.style in ("google", "pep8"):
-            true_from_level = getattr(node, "level", -1)
-
-            if true_from_level == -1:
-                from_level = 0
-                is_not_star_import = False
+            module = node.module or ''
+            if node.level > 0:
+                type_ = IMPORT_APP_RELATIVE
             else:
-                from_level = true_from_level
-                is_not_star_import = (
-                    not any(nm.endswith("*")
-                            for nm, asnm in imported_names)
-                )
-
-        else:
-            from_level = getattr(node, "level", -1)
-            is_not_star_import = (
-                not any(nm.endswith("*")
-                        for nm, asnm in imported_names)
+                type_ = self._classify_type(module)
+            names = [alias.name for alias in node.names]
+            classified_import = ClassifiedImport(
+                type_, True, [module], names,
+                node.lineno, node.level,
+                root_package_name(module),
             )
+            self.imports.append(classified_import)
 
-        if self.style == 'pep8':
-            names = imported_names = ['']
-            from_level = is_not_star_import = None
+    def _classify_type(self, module):
+        package = root_package_name(module)
 
-        n = (
-            import_type,
-            names,
-            from_level,
-            is_not_star_import,
-            imported_names,
-        )
-
-        if n[0] == IMPORT_FUTURE:
-            group = (n[0], None, None, None, n[4])
-        elif (
-            n[0] in (IMPORT_STDLIB, IMPORT_APP_RELATIVE) or
-            self.style != 'cryptography'
-        ):
-            group = (n[0], n[2], n[1], n[3], n[4])
-        elif n[0] == IMPORT_3RD_PARTY:
-            group = (n[0], n[1], n[2], n[3], n[4])
-        else:
-            group = n
-
-        return group, n
-
-    def _import_type(self, node, name):
-        if isinstance(name, int):
-            return None
-
-        if name is None:
-            # relative import
-            return IMPORT_APP
-
-        pkg = root_package_name(name)
-
-        if pkg == "__future__":
+        if package == "__future__":
             return IMPORT_FUTURE
-
-        elif pkg in self.application_import_names:
+        elif package in self.application_import_names:
             return IMPORT_APP
-
-        elif isinstance(node, ast.ImportFrom) and node.level > 0:
-            return IMPORT_APP_RELATIVE
-
-        elif pkg in STDLIB_NAMES:
+        elif package in STDLIB_NAMES:
             return IMPORT_STDLIB
-
         else:
             # Not future, stdlib or an application import.
             # Must be 3rd party.
             return IMPORT_3RD_PARTY
-
-
-class ImportOrderChecker(object):
-    visitor_class = ImportVisitor
-    options = None
-
-    def __init__(self, filename, tree):
-        self.tree = tree
-        self.filename = filename
-        self.lines = None
-
-    def load_file(self):
-        if self.filename in ("stdin", "-", None):
-            self.filename = "stdin"
-            self.lines = pycodestyle.stdin_get_value().splitlines(True)
-        else:
-            self.lines = pycodestyle.readlines(self.filename)
-
-        if not self.tree:
-            self.tree = ast.parse("".join(self.lines))
-
-    def error(self, node, code, message):
-        raise NotImplemented()
-
-    def check_order(self):
-        if not self.tree or not self.lines:
-            self.load_file()
-
-        visitor = self.visitor_class(self.filename, self.options)
-        visitor.visit(self.tree)
-
-        style = self.options['import_order_style']
-
-        prev_node = None
-        for node in visitor.imports:
-            # Lines with the noqa flag are ignored entirely
-            if pycodestyle.noqa(self.lines[node.lineno - 1]):
-                continue
-
-            n, k = visitor.node_sort_key(node)
-
-            cmp_n = cmp_values(n, style)
-
-            if cmp_n[-1] and not is_sorted(cmp_n[-1]):
-                should_be = sorted_import_names(n[-1], style)
-                yield self.error(
-                    node, "I101",
-                    (
-                        "Imported names are in the wrong order. "
-                        "Should be {0}".format(should_be)
-                    )
-                )
-
-            if prev_node is None:
-                prev_node = node
-                continue
-
-            pn, pk = visitor.node_sort_key(prev_node)
-
-            cmp_pn = cmp_values(pn, style)
-
-            # FUTURES
-            # STDLIBS, STDLIB_FROMS
-            # 3RDPARTY[n], 3RDPARTY_FROM[n]
-            # 3RDPARTY[n+1], 3RDPARTY_FROM[n+1]
-            # APPLICATION, APPLICATION_FROM
-
-            # import_type, names, level, is_star_import, imported_names,
-
-            if n[0] == IMPORT_MIXED:
-                yield self.error(
-                    node, "I666",
-                    "Import statement mixes groups"
-                )
-                prev_node = node
-                continue
-
-            if cmp_n < cmp_pn:
-                def build_str(key):
-                    level = key[2]
-                    if level >= 0:
-                        start = "from " + level * '.'
-                    else:
-                        start = "import "
-                    return start + ", ".join(key[1])
-
-                first_str = build_str(k)
-                second_str = build_str(pk)
-
-                yield self.error(
-                    node, "I100",
-                    (
-                        "Import statements are in the wrong order. "
-                        "{0} should be before {1}".format(
-                            first_str,
-                            second_str
-                        )
-                    )
-                )
-
-            lines_apart = node.lineno - prev_node.lineno
-
-            is_app = (
-                set([cmp_n[0], cmp_pn[0]]) !=
-                set([IMPORT_APP, IMPORT_APP_RELATIVE])
-            )
-
-            if lines_apart == 1 and ((
-                cmp_n[0] != cmp_pn[0] and
-                (style == 'cryptography' or is_app)
-            ) or (
-                n[0] == IMPORT_3RD_PARTY and
-                style == 'cryptography' and
-                root_package_name(cmp_n[1][0]) !=
-                root_package_name(cmp_pn[1][0])
-            )):
-                yield self.error(
-                    node, "I201",
-                    "Missing newline before sections or imports."
-                )
-
-            prev_node = node

flake8_import_order/checker.py
@@ -1,0 +1,64 @@
+import ast
+
+import pycodestyle
+
+from flake8_import_order import ImportVisitor
+from flake8_import_order.styles import (
+    Cryptography, Google, PEP8, Smarkets,
+)
+
+DEFAULT_IMPORT_ORDER_STYLE = 'cryptography'
+
+
+class ImportOrderChecker(object):
+    visitor_class = ImportVisitor
+    options = None
+
+    def __init__(self, filename, tree):
+        self.tree = tree
+        self.filename = filename
+        self.lines = None
+
+    def load_file(self):
+        if self.filename in ("stdin", "-", None):
+            self.filename = "stdin"
+            self.lines = pycodestyle.stdin_get_value().splitlines(True)
+        else:
+            self.lines = pycodestyle.readlines(self.filename)
+
+        if not self.tree:
+            self.tree = ast.parse("".join(self.lines))
+
+    def error(self, error):
+        raise NotImplemented()
+
+    def check_order(self):
+        if not self.tree or not self.lines:
+            self.load_file()
+
+        visitor = self.visitor_class(
+            self.options.get('application_import_names', []),
+        )
+        visitor.visit(self.tree)
+
+        imports = []
+        for import_ in visitor.imports:
+            if not pycodestyle.noqa(self.lines[import_.lineno - 1]):
+                imports.append(import_)
+
+        style_option = self.options.get(
+            'import_order_style', DEFAULT_IMPORT_ORDER_STYLE,
+        )
+        if style_option == 'cryptography':
+            style = Cryptography(imports)
+        elif style_option == 'google':
+            style = Google(imports)
+        elif style_option == 'pep8':
+            style = PEP8(imports)
+        elif style_option == 'smarkets':
+            style = Smarkets(imports)
+        else:
+            raise AssertionError("Unknown style {)".format(style_option))
+
+        for error in style.check():
+            yield self.error(error)

flake8_import_order/flake8_linter.py
@@ -2,13 +2,15 @@ from __future__ import absolute_import
 
 import optparse
 
-import flake8_import_order
-from flake8_import_order import DEFAULT_IMPORT_ORDER_STYLE, ImportOrderChecker
+from flake8_import_order import __version__
+from flake8_import_order.checker import (
+    DEFAULT_IMPORT_ORDER_STYLE, ImportOrderChecker,
+)
 
 
 class Linter(ImportOrderChecker):
     name = "import-order"
-    version = flake8_import_order.__version__
+    version = __version__
 
     def __init__(self, tree, filename):
         super(Linter, self).__init__(filename, tree)
@@ -52,9 +54,13 @@ class Linter(ImportOrderChecker):
 
         cls.options = optdict
 
-    def error(self, node, code, message):
-        lineno, col_offset = node.lineno, node.col_offset
-        return (lineno, col_offset, '{0} {1}'.format(code, message), Linter)
+    def error(self, error):
+        return (
+            error.lineno,
+            0,
+            "{0} {1}".format(error.code, error.message),
+            Linter,
+        )
 
     def run(self):
         for error in self.check_order():

flake8_import_order/pylama_linter.py
@@ -2,12 +2,15 @@ from __future__ import absolute_import
 
 from pylama.lint import Linter as BaseLinter
 
-from flake8_import_order import DEFAULT_IMPORT_ORDER_STYLE, ImportOrderChecker
+from flake8_import_order import __version__
+from flake8_import_order.checker import (
+    DEFAULT_IMPORT_ORDER_STYLE, ImportOrderChecker,
+)
 
 
 class Linter(ImportOrderChecker, BaseLinter):
     name = "import-order"
-    version = "0.1"
+    version = __version__
 
     def __init__(self):
         super(Linter, self).__init__(None, None)
@@ -15,13 +18,12 @@ class Linter(ImportOrderChecker, BaseLinter):
     def allow(self, path):
         return path.endswith(".py")
 
-    def error(self, node, code, message):
-        lineno, col_offset = node.lineno, node.col_offset
+    def error(self, error):
         return {
-            "lnum": lineno,
-            "col": col_offset,
-            "text": message,
-            "type": code
+            'lnum': error.lineno,
+            'col': 0,
+            'text': error.message,
+            'type': error.code,
         }
 
     def run(self, path, **meta):
@@ -29,7 +31,8 @@ class Linter(ImportOrderChecker, BaseLinter):
         self.tree = None
         self.options = dict(
             {'import_order_style': DEFAULT_IMPORT_ORDER_STYLE},
-            **meta)
+            **meta
+        )
 
         for error in self.check_order():
             yield error

tests/test_cases/complete_with_bad_order.py
@@ -17,8 +17,8 @@ from Y import A
 from Y import B, D, C # I101
 
 from Z import A
-from Z.A.B import A # I100
-from Z.A import A
+from Z.A.B import A
+from Z.A import A # I100
 
 import flake8_import_error
 from flake8_import_error import *

tox.ini
@@ -1,5 +1,5 @@
 [tox]
-envlist = {py26,py27,py33,py34,py35}-flake8{2,3},pypy,pep8,py3pep8
+envlist = {py27,py33,py34,py35}-flake8{2,3},pypy,pep8,py3pep8
 
 [testenv]
 deps =

commit 3f371dded0b19dba24e0287786fc615cfe3624cd

Fix module ordering case sensitivity
Both Google and Smarkets sytle should ignore the case of the module
when considering the ordering of it. This fixes a bug introduced
in 7c04e334c6b03727e7dcc5cb50535fea14044ce6.

flake8_import_order/styles.py
@@ -90,7 +90,8 @@ class Google(Style):
 
     @staticmethod
     def key(import_):
-        return (import_.type, import_.level, import_.modules)
+        modules = [module.lower() for module in import_.modules]
+        return (import_.type, import_.level, modules)
 
 
 class Smarkets(Style):
@@ -101,7 +102,8 @@ class Smarkets(Style):
 
     @staticmethod
     def key(import_):
-        return (import_.type, import_.is_from, import_.level, import_.modules)
+        modules = [module.lower() for module in import_.modules]
+        return (import_.type, import_.is_from, import_.level, modules)
 
 
 class Cryptography(Style):

tests/test_cases/complete_google.py
@@ -4,6 +4,7 @@ import ast
 from functools import *
 import os
 from os import path
+import StringIO
 import sys
 
 import X

tests/test_cases/complete_smarkets.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import ast
 import os
+import StringIO
 import sys
 from functools import *
 from os import path