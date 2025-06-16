fe225640b39b38e57d03aae1b61b4d8c44bf754e
Ian Stapleton Cordasco <graffatcolmingov@gmail.com>
2025-06-16 16:21:27 -0500

    Fix for AttributeError in if statement
    Split test cases out for ``t.TYPE_CHECKING`` and
    ``typing.TYPE_CHECKING`` to avoid false positive linter errors.
    
    Closes #212

--- tests/test_cases/complete_cryptography_alt0.py
+++ tests/test_cases/complete_cryptography_alt0.py
@@ -1,4 +1,4 @@
-# edited
+# cryptography
 
 import ast
 import os
@@ -7,25 +7,30 @@ from functools import *
 from os import path
 
 import X
-import Y
-import Z
 from X import *
 from X import A
-from X import B, b, C, d
+from X import B, C, D
+
+import Y
 from Y import *
 from Y import A
 from Y import B, C, D
-from Y import e
+
+import Z
 from Z import A
 from Z.A import A
 from Z.A.B import A
 
 import localpackage
 
-from localpackage import A, b
-
 import flake8_import_order
 from flake8_import_order import *
+from flake8_import_order import A
+from flake8_import_order import B
+
+import tests
+from tests import A
+from tests import B
 
 from . import A
 from . import B
@@ -36,7 +41,7 @@ from .. import B
 from ..A import A
 from ..B import B
 
-if TYPE_CHECKING:
+if typing.TYPE_CHECKING:
     import ast
     import os
     import sys
@@ -44,25 +49,30 @@ if TYPE_CHECKING:
     from os import path
 
     import X
-    import Y
-    import Z
     from X import *
     from X import A
-    from X import B, b, C, d
+    from X import B, C, D
+
+    import Y
     from Y import *
     from Y import A
     from Y import B, C, D
-    from Y import e
+
+    import Z
     from Z import A
     from Z.A import A
     from Z.A.B import A
 
     import localpackage
 
-    from localpackage import A, b
-
     import flake8_import_order
     from flake8_import_order import *
+    from flake8_import_order import A
+    from flake8_import_order import B
+
+    import tests
+    from tests import A
+    from tests import B
 
     from . import A
     from . import B
